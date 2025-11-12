49068f7f13af5512eb02bd1e1872d7833bdd9d55
Oscorp36 <oscar366313@gmail.com>
2025-11-12 12:57:36 -0800

    Add Marzipano tour files

---
+++ data.js
@@ -0,0 +1,829 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01_-entrance",
+      "name": "01_ entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.22678163529097972,
+        "pitch": 0.038759399516706594,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.018360207474526,
+          "pitch": 0.13575773916771539,
+          "rotation": 0,
+          "target": "1-02_-services-desk"
+        },
+        {
+          "yaw": -2.2187540799501377,
+          "pitch": 0.2625829682227643,
+          "rotation": 0.7853981633974483,
+          "target": "2-03_printer-stations"
+        },
+        {
+          "yaw": -1.6269852466518486,
+          "pitch": 0.30586781171741073,
+          "rotation": 0,
+          "target": "6-08_children-desk"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02_-services-desk",
+      "name": "02_ services desk",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.01462371827315323,
+          "pitch": 0.21680331624671645,
+          "rotation": 0,
+          "target": "0-01_-entrance"
+        },
+        {
+          "yaw": 1.577496991069756,
+          "pitch": 0.46723218614019935,
+          "rotation": 0,
+          "target": "3-04_gallery"
+        },
+        {
+          "yaw": 2.7460098969896958,
+          "pitch": 0.07814748146791572,
+          "rotation": 5.497787143782138,
+          "target": "8-09_2f-atrium"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03_printer-stations",
+      "name": "03_printer stations",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.384838284944202,
+          "pitch": 0.2522007610118635,
+          "title": "Printing and copier stations<div><br></div>",
+          "text": "https://longbeach.gov/library/learn/computers-printing/"
+        }
+      ]
+    },
+    {
+      "id": "3-04_gallery",
+      "name": "04_gallery",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.0391988749127705,
+        "pitch": 0.06591466840249716,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1693998764567066,
+          "pitch": 0.2746452135098938,
+          "rotation": 0,
+          "target": "1-02_-services-desk"
+        },
+        {
+          "yaw": -2.222406692015836,
+          "pitch": 0.34615692546190324,
+          "rotation": 5.497787143782138,
+          "target": "4-05_miller-room"
+        },
+        {
+          "yaw": 1.9014962801095727,
+          "pitch": 0.1349544955143287,
+          "rotation": 0.7853981633974483,
+          "target": "8-09_2f-atrium"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05_miller-room",
+      "name": "05_Miller room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 1.1655496887098593,
+        "pitch": 0.012932519377196527,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2664860580713935,
+          "pitch": 0.24676410395647608,
+          "rotation": 0.7853981633974483,
+          "target": "1-02_-services-desk"
+        },
+        {
+          "yaw": -2.8182724149688134,
+          "pitch": 0.23685484764811982,
+          "rotation": 0,
+          "target": "0-01_-entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06_children-hallway",
+      "name": "06_Children hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.063738430040317,
+        "pitch": 0.08942527836233793,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-08_children-desk",
+      "name": "08_children desk",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.34875061693691123,
+        "pitch": -0.046511279420037965,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.094048273655165,
+          "pitch": 0.3550344144967781,
+          "rotation": 0,
+          "target": "0-01_-entrance"
+        },
+        {
+          "yaw": -2.6328853723510157,
+          "pitch": 0.1598421435704953,
+          "rotation": 0.7853981633974483,
+          "target": "1-02_-services-desk"
+        },
+        {
+          "yaw": -1.0477630854006534,
+          "pitch": 0.20625593832110312,
+          "rotation": 5.497787143782138,
+          "target": "7-07_-children-space"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-07_-children-space",
+      "name": "07_ children space",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.8584875498418931,
+        "pitch": 0.26545098673935286,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5081357494983365,
+          "pitch": 0.2968824592497743,
+          "rotation": 0,
+          "target": "6-08_children-desk"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-09_2f-atrium",
+      "name": "09_2F atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.26966051591322326,
+        "pitch": -0.06459899919451217,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4482594879420336,
+          "pitch": 0.23664546242521034,
+          "rotation": 0,
+          "target": "9-10_-2f-printer-station"
+        },
+        {
+          "yaw": 1.506419886861588,
+          "pitch": 0.3526439540019304,
+          "rotation": 0,
+          "target": "13-14_-2f-study-area-1"
+        },
+        {
+          "yaw": 2.9187672790207975,
+          "pitch": 0.08896382550380899,
+          "rotation": 0,
+          "target": "14-15_-main-study-area"
+        },
+        {
+          "yaw": -2.8122833842426704,
+          "pitch": 0.07141518329520125,
+          "rotation": 0.7853981633974483,
+          "target": "16-r0010102"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-10_-2f-printer-station",
+      "name": "10_ 2F printer station",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.780395627324852,
+        "pitch": 0.016795739790570252,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5944583042434308,
+          "pitch": 0.35282913595627363,
+          "rotation": 0,
+          "target": "10-11_-2f-teen-space-entrance"
+        },
+        {
+          "yaw": -1.555544038857093,
+          "pitch": 0.2813988309195441,
+          "rotation": 0,
+          "target": "8-09_2f-atrium"
+        },
+        {
+          "yaw": 1.3168460463999327,
+          "pitch": 0.3075953503884108,
+          "rotation": 5.497787143782138,
+          "target": "12-13_-2f-hallway1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.780395627324852,
+          "pitch": 0.016795739790570252,
+          "title": "Printer and copier stations",
+          "text": "https://longbeach.gov/library/learn/computers-printing/"
+        }
+      ]
+    },
+    {
+      "id": "10-11_-2f-teen-space-entrance",
+      "name": "11_ 2F Teen Space entrance",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.8451977529540855,
+        "pitch": 0.06847493914617608,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.2721364976602185,
+          "pitch": 0.5507396787589443,
+          "rotation": 0,
+          "target": "11-12_-2f-teen-space"
+        },
+        {
+          "yaw": 2.8982129777188366,
+          "pitch": 0.16669410533737583,
+          "rotation": 0,
+          "target": "12-13_-2f-hallway1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-12_-2f-teen-space",
+      "name": "12_ 2F Teen space",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.7899468462252628,
+        "pitch": -0.06851575827434964,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.164576164017287,
+          "pitch": 0.3583090296876925,
+          "rotation": 0,
+          "target": "10-11_-2f-teen-space-entrance"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-13_-2f-hallway1",
+      "name": "13_ 2F hallway1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.5822095364241076,
+        "pitch": -0.07621909020939377,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.12067686722955528,
+          "pitch": 0.1779531688203413,
+          "rotation": 0,
+          "target": "11-12_-2f-teen-space"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-14_-2f-study-area-1",
+      "name": "14_ 2F study area 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.7460900756272046,
+        "pitch": -0.04263533946837583,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.08330235798906038,
+          "pitch": 0.2133719184985985,
+          "rotation": 0,
+          "target": "12-13_-2f-hallway1"
+        },
+        {
+          "yaw": 0.5839120938055338,
+          "pitch": 0.21103082756000902,
+          "rotation": 0.7853981633974483,
+          "target": "8-09_2f-atrium"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-15_-main-study-area",
+      "name": "15_ main study area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.19153661186513737,
+        "pitch": -0.029721475767903627,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06332067395037555,
+          "pitch": 0.0754977782437507,
+          "rotation": 0,
+          "target": "8-09_2f-atrium"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-17_-study-area-2",
+      "name": "17_ study area 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.395438565738484,
+        "pitch": -0.01421177982279076,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.49838101709020677,
+          "pitch": 0.11152662050151285,
+          "rotation": 5.497787143782138,
+          "target": "8-09_2f-atrium"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-r0010102",
+      "name": "R0010102",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.6097753189063049,
+        "pitch": -0.1020887768114811,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5499509112468868,
+          "pitch": 0.30518941721572723,
+          "rotation": 5.497787143782138,
+          "target": "8-09_2f-atrium"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "17-r0010104",
+      "name": "R0010104",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.5679069522412945,
+        "pitch": 0.10852631864677065,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.601589653248679,
+          "pitch": 0.16364417488840566,
+          "rotation": 5.497787143782138,
+          "target": "8-09_2f-atrium"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "18-r0010106",
+      "name": "R0010106",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 3.0491885464806865,
+        "pitch": -0.0516791993556005,
+        "fov": 1.444433621989194
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.12297529224532,
+          "pitch": 0.11219886090248643,
+          "rotation": 0,
+          "target": "8-09_2f-atrium"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
